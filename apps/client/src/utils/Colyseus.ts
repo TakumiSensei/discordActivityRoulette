61e946121cf07f603a42ca1ec3c1824204f4abec
TakumiSensei <tkmmnkr@gmail.com>
2025-07-24 22:47:09 +0900

    マルチプレイ対応

--- apps/client/src/utils/Colyseus.ts
+++ apps/client/src/utils/Colyseus.ts
@@ -2,4 +2,8 @@ import { Client } from "colyseus.js";
 
 // export const colyseusSDK = new Client('https://meet-hunt-kazakhstan-attraction.trycloudflare.com');
 // export const colyseusSDK = new Client('https://de-fra-90499d00.colyseus.dev');
-export const colyseusSDK = new Client('/colyseus');
+// export const colyseusSDK = new Client('/colyseus');
+const queryParams = new URLSearchParams(window.location.search);
+const isEmbedded = queryParams.get('frame_id') != null;
+
+export const colyseusSDK = new Client(isEmbedded ? '/.proxy/colyseus' : '/colyseus');
